e2a4bec7e9d955a495d89514998fb0eeef3d8c20
artemmokinn <artemmokinn@gmail.com>
2021-10-29 15:36:11 +0500

    first commit

---
+++ decryption.py
@@ -0,0 +1,34 @@
+import pyAesCrypt
+import os
+
+
+def decryption(file, password):
+    buffer_size = 512 * 1024
+
+    pyAesCrypt.decryptFile(
+        str(file),
+        str(os.path.splitext(file)[0]),
+        password,
+        buffer_size
+    )
+
+    print("[File '" + str(os.path.splitext(file)[0]) + "' was decrypted]")
+
+    os.remove(file)
+
+
+def walking_by_dirs(dir, password):
+    for name in os.listdir(dir):
+        path = os.path.join(dir, name)
+
+        if os.path.isfile(path):
+            try:
+                decryption(path, password)
+            except Exception as ex:
+                print(ex)
+        else:
+            walking_by_dirs(path, password)
+
+
+password = input('Enter the password for decryption: ')
+walking_by_dirs('', password)  # path to the file
---
+++ encryption.py
@@ -0,0 +1,34 @@
+import pyAesCrypt
+import os
+
+
+def encryption(file, password):
+    buffer_size = 512 * 1024
+
+    pyAesCrypt.encryptFile(
+        str(file),
+        str(file) + '.crp',
+        password,
+        buffer_size
+    )
+
+    print("[File '" + str(os.path.splitext(file)[0]) + "' was encrypted]")
+
+    os.remove(file)
+
+
+def walking_by_dirs(dir, password):
+    for name in os.listdir(dir):
+        path = os.path.join(dir, name)
+
+        if os.path.isfile(path):
+            try:
+                encryption(path, password)
+            except Exception as ex:
+                print(ex)
+        else:
+            walking_by_dirs(path, password)
+
+
+password = input('Enter the password for encryption: ')
+walking_by_dirs('', password)  # path to the file
